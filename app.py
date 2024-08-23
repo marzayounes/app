537a31fbe0adced484bf880797702bfa73df07f3
Mariem <mariem.youssef-bouhlel@sobre-energie.com>
2024-08-23 13:31:03 +0200

    nouveau fichier app

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ basepath = os.path.join(current_folder, "Models")
 X_test = pd.read_csv(os.path.join(basepath, "X_test_sample.csv"))
 #commenter y_test 
 #y_test = pd.read_csv(os.path.join(basepath, "y_test_sample.csv"))
-explainer = joblib.load(os.path.join(basepath, "explainer"))
+#explainer = joblib.load(os.path.join(basepath, "explainer"))
 shap_values = pd.read_csv(os.path.join(basepath, "shap_values_sample.csv"))
 shap_values1 = pd.read_csv(os.path.join(basepath, "shap_values1_sample.csv"))
-expected_value = joblib.load(os.path.join(basepath, "expected_values.pkl"))
+#expected_value = joblib.load(os.path.join(basepath, "expected_values.pkl"))
 model_load = joblib.load(os.path.join(basepath, "model.pkl"))
 best_thresh = joblib.load(os.path.join(basepath, "best_thresh_LightGBM_NS.pkl"))
 
